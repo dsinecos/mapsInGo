f3a024bb2231b7df7216644d7f2ca5752a90500c
Divyanshu Singh <dsinecos@gmail.com>
2019-03-13 12:00:47 +0530

    iterating over maps

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	// fmt.Println("Using Maps in Go")
-
-	// Different ways to declare Maps
-	// 1.
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-
-	// 2.
-	// var colors map[string]string
-
-	// 3.
-	// colors := make(map[string]string)
-	// colors["white"] = "#ffffff"
-
-	// Deleting keys in Maps
-	// delete(colors, "white")
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
 	// fmt.Println(colors)
 
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color + " : " + hex)
+	}
 }
