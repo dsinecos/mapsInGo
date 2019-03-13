26233c011a57d6fff48edbed72b2bf673e25396f
Divyanshu Singh <dsinecos@gmail.com>
2019-03-13 11:38:42 +0530

    three ways to declare maps in go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	// fmt.Println("Using Maps in Go")
+
+	// Different ways to declare Maps
+	// 1.
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// 2.
+	// var colors map[string]string
+
+	// 3.
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+
+	// Deleting keys in Maps
+	// delete(colors, "white")
+
+	// fmt.Println(colors)
+
+}
